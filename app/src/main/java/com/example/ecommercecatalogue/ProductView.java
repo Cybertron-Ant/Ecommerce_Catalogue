a097175d3419a245a7e2edea58a674a0baa9d51f
Antonio Fuller <mediainformationofficer@gmail.com>
2024-02-02 09:08:03 -0500

    create product_view.xml

--- app/src/main/java/com/example/ecommercecatalogue/ProductView.java
+++ app/src/main/java/com/example/ecommercecatalogue/ProductView.java
@@ -1,6 +1,16 @@
 package com.example.ecommercecatalogue;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProductView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.product_view);
+    }
 }// end 'ProductView' class

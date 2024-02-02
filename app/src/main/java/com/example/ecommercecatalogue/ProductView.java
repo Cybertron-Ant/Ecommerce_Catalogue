ea99ce3868131a124eec7ff47a799e54dbd96016
Antonio Fuller <mediainformationofficer@gmail.com>
2024-02-02 09:25:15 -0500

    reference id of the XML elements

--- app/src/main/java/com/example/ecommercecatalogue/ProductView.java
+++ app/src/main/java/com/example/ecommercecatalogue/ProductView.java
@@ -1,16 +1,29 @@
 package com.example.ecommercecatalogue;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProductView extends AppCompatActivity {
 
+    private ImageView productViewImage;
+    private TextView productViewDescription;
+    private TextView descriptionText;
+    private TextView specificationTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.product_view);
+
+        // initialize variables with references to the XML elements
+        productViewImage = findViewById(R.id.productViewImage);
+        productViewDescription = findViewById(R.id.productViewDescription);
+        descriptionText = findViewById(R.id.descriptionText);
+        specificationTextView = findViewById(R.id.specificationTextView);
+
     }
 }// end 'ProductView' class
